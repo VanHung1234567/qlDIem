16a9c118f5eeb44328c1f40c5f60fb4967f122be
Van Hung <hungdongky2003@gmail.com>
2024-11-08 09:14:10 +0700

    1

---
+++ qlDiem/TrangChu.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace qlDiem
+{
+    partial class TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrangChu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnSinhVien = new System.Windows.Forms.Button();
+            this.btnMonHoc = new System.Windows.Forms.Button();
+            this.btnTrangChu = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnHocKy = new System.Windows.Forms.Button();
+            this.btnLopHoc = new System.Windows.Forms.Button();
+            this.btnDkyKhoaHoc = new System.Windows.Forms.Button();
+            this.btnDiem = new System.Windows.Forms.Button();
+            this.btnChuyenNganh = new System.Windows.Forms.Button();
+            this.btnLoaiKhoaHoc = new System.Windows.Forms.Button();
+            this.btnLoaiDauDiem = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Controls.Add(this.btnLoaiDauDiem);
+            this.panel1.Controls.Add(this.btnLoaiKhoaHoc);
+            this.panel1.Controls.Add(this.btnChuyenNganh);
+            this.panel1.Controls.Add(this.btnDiem);
+            this.panel1.Controls.Add(this.btnDkyKhoaHoc);
+            this.panel1.Controls.Add(this.btnLopHoc);
+            this.panel1.Controls.Add(this.btnHocKy);
+            this.panel1.Controls.Add(this.btnThoat);
+            this.panel1.Controls.Add(this.btnSinhVien);
+            this.panel1.Controls.Add(this.btnMonHoc);
+            this.panel1.Controls.Add(this.btnTrangChu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 554);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnThoat.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnThoat.Location = new System.Drawing.Point(32, 505);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(108, 32);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Exit";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnSinhVien
+            // 
+            this.btnSinhVien.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSinhVien.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSinhVien.Location = new System.Drawing.Point(32, 73);
+            this.btnSinhVien.Name = "btnSinhVien";
+            this.btnSinhVien.Size = new System.Drawing.Size(108, 32);
+            this.btnSinhVien.TabIndex = 6;
+            this.btnSinhVien.Text = "Student";
+            this.btnSinhVien.UseVisualStyleBackColor = false;
+            this.btnSinhVien.Click += new System.EventHandler(this.btnSinhVien_Click);
+            // 
+            // btnMonHoc
+            // 
+            this.btnMonHoc.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMonHoc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMonHoc.Location = new System.Drawing.Point(32, 121);
+            this.btnMonHoc.Name = "btnMonHoc";
+            this.btnMonHoc.Size = new System.Drawing.Size(108, 32);
+            this.btnMonHoc.TabIndex = 5;
+            this.btnMonHoc.Text = "Subject";
+            this.btnMonHoc.UseVisualStyleBackColor = false;
+            this.btnMonHoc.Click += new System.EventHandler(this.btnMonHoc_Click);
+            // 
+            // btnTrangChu
+            // 
+            this.btnTrangChu.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnTrangChu.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTrangChu.Location = new System.Drawing.Point(32, 25);
+            this.btnTrangChu.Name = "btnTrangChu";
+            this.btnTrangChu.Size = new System.Drawing.Size(108, 32);
+            this.btnTrangChu.TabIndex = 0;
+            this.btnTrangChu.Text = "Home";
+            this.btnTrangChu.UseVisualStyleBackColor = false;
+            this.btnTrangChu.Click += new System.EventHandler(this.btnTrangChu_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblTittle);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(180, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(862, 110);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(45, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contact: 0336790864";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(45, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Address: 218 Lĩnh Nam - Vĩnh Hưng - Hoàng Mai - Hà Nội";
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lblTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTittle.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTittle.Location = new System.Drawing.Point(228, 9);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(292, 25);
+            this.lblTittle.TabIndex = 0;
+            this.lblTittle.Text = "System Management Point\r\n";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(180, 110);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(862, 444);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(862, 444);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnHocKy
+            // 
+            this.btnHocKy.Location = new System.Drawing.Point(32, 169);
+            this.btnHocKy.Name = "btnHocKy";
+            this.btnHocKy.Size = new System.Drawing.Size(108, 32);
+            this.btnHocKy.TabIndex = 8;
+            this.btnHocKy.Text = "Semester";
+            this.btnHocKy.UseVisualStyleBackColor = true;
+            this.btnHocKy.Click += new System.EventHandler(this.btnHocKy_Click);
+            // 
+            // btnLopHoc
+            // 
+            this.btnLopHoc.Location = new System.Drawing.Point(32, 217);
+            this.btnLopHoc.Name = "btnLopHoc";
+            this.btnLopHoc.Size = new System.Drawing.Size(108, 32);
+            this.btnLopHoc.TabIndex = 9;
+            this.btnLopHoc.Text = "Classes";
+            this.btnLopHoc.UseVisualStyleBackColor = true;
+            this.btnLopHoc.Click += new System.EventHandler(this.btnLopHoc_Click);
+            // 
+            // btnDkyKhoaHoc
+            // 
+            this.btnDkyKhoaHoc.Location = new System.Drawing.Point(32, 265);
+            this.btnDkyKhoaHoc.Name = "btnDkyKhoaHoc";
+            this.btnDkyKhoaHoc.Size = new System.Drawing.Size(108, 32);
+            this.btnDkyKhoaHoc.TabIndex = 10;
+            this.btnDkyKhoaHoc.Text = "Register for Class";
+            this.btnDkyKhoaHoc.UseVisualStyleBackColor = true;
+            this.btnDkyKhoaHoc.Click += new System.EventHandler(this.btnDkyKhoaHoc_Click);
+            // 
+            // btnDiem
+            // 
+            this.btnDiem.Location = new System.Drawing.Point(32, 313);
+            this.btnDiem.Name = "btnDiem";
+            this.btnDiem.Size = new System.Drawing.Size(108, 32);
+            this.btnDiem.TabIndex = 11;
+            this.btnDiem.Text = "Scores";
+            this.btnDiem.UseVisualStyleBackColor = true;
+            this.btnDiem.Click += new System.EventHandler(this.btnDiem_Click);
+            // 
+            // btnChuyenNganh
+            // 
+            this.btnChuyenNganh.Location = new System.Drawing.Point(32, 361);
+            this.btnChuyenNganh.Name = "btnChuyenNganh";
+            this.btnChuyenNganh.Size = new System.Drawing.Size(108, 32);
+            this.btnChuyenNganh.TabIndex = 12;
+            this.btnChuyenNganh.Text = "Major";
+            this.btnChuyenNganh.UseVisualStyleBackColor = true;
+            this.btnChuyenNganh.Click += new System.EventHandler(this.btnChuyenNganh_Click);
+            // 
+            // btnLoaiKhoaHoc
+            // 
+            this.btnLoaiKhoaHoc.Location = new System.Drawing.Point(32, 409);
+            this.btnLoaiKhoaHoc.Name = "btnLoaiKhoaHoc";
+            this.btnLoaiKhoaHoc.Size = new System.Drawing.Size(108, 32);
+            this.btnLoaiKhoaHoc.TabIndex = 13;
+            this.btnLoaiKhoaHoc.Text = "Courses Category";
+            this.btnLoaiKhoaHoc.UseVisualStyleBackColor = true;
+            this.btnLoaiKhoaHoc.Click += new System.EventHandler(this.btnLoaiKhoaHoc_Click);
+            // 
+            // btnLoaiDauDiem
+            // 
+            this.btnLoaiDauDiem.Location = new System.Drawing.Point(32, 457);
+            this.btnLoaiDauDiem.Name = "btnLoaiDauDiem";
+            this.btnLoaiDauDiem.Size = new System.Drawing.Size(108, 32);
+            this.btnLoaiDauDiem.TabIndex = 14;
+            this.btnLoaiDauDiem.Text = "Assessment Type";
+            this.btnLoaiDauDiem.UseVisualStyleBackColor = true;
+            this.btnLoaiDauDiem.Click += new System.EventHandler(this.btnLoaiDauDiem_Click);
+            // 
+            // TrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 554);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "TrangChu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrangChu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TrangChu_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTittle;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnTrangChu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnSinhVien;
+        private System.Windows.Forms.Button btnMonHoc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoaiKhoaHoc;
+        private System.Windows.Forms.Button btnChuyenNganh;
+        private System.Windows.Forms.Button btnDiem;
+        private System.Windows.Forms.Button btnDkyKhoaHoc;
+        private System.Windows.Forms.Button btnLopHoc;
+        private System.Windows.Forms.Button btnHocKy;
+        private System.Windows.Forms.Button btnLoaiDauDiem;
+    }
+}
---
+++ qlDiem/TrangChu.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qlDiem
+{
+    public partial class TrangChu : Form
+    {
+        public TrangChu()
+        {
+            InitializeComponent();
+        }
+
+        private Form currentFormChild;
+
+        private void OpenChildForm(Form childForm, string title)
+        {
+            if (currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+            currentFormChild = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel3.Controls.Add(childForm);
+            panel3.Tag = childForm;
+            panel3.AutoScroll = true;
+
+            childForm.BringToFront();
+            childForm.Show();
+
+            lblTittle.Text = title;
+        }
+
+        private void btnTrangChu_Click(object sender, EventArgs e)
+        {
+            if (currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+            lblTittle.Text = "Home";
+        }
+
+        private void btnSinhVien_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new SinhVien(), "Student");
+
+        }
+
+        private void btnMonHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new MonHoc(), "Subject");
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TrangChu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn muốn đóng giao diện không?",
+                "Xác nhận",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnHocKy_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new HocKy(), "Semester");
+        }
+
+        private void btnLopHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new LopHoc(), "Classes");
+        }
+
+        private void btnDkyKhoaHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new DkyLopHoc(), "Register for class");
+        }
+
+        private void btnDiem_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Diem(), "Scores");
+        }
+
+        private void btnChuyenNganh_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new ChuyenNganh(), "Major");
+        }
+
+        private void btnLoaiKhoaHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new LoaiKhoaHoc(), "Courses Categories");
+        }
+
+        private void btnLoaiDauDiem_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new LoaiDauDiem(), "Assessment Type");
+        }
+    }
+}
